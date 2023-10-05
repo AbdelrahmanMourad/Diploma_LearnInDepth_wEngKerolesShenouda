da9f6d47379d87f1e9d548f83c2ef43823a72d5f
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-10-05 16:14:16 +0300

    Pass Struct To Function By Value and By Reference

--- Unit_2_C_Programming/Lesson_6_Pointers/Labs/Lab10_WordSize_FromStruct.c
+++ Unit_2_C_Programming/Lesson_6_Pointers/Labs/Lab10_WordSize_FromStruct.c
@@ -15,13 +15,26 @@ struct TEST3
 };
 struct TEST4
 {
-    char m_day;
-    double m_day2;
+    char x1;
+    short x2;
+    int x3;
+    double x4;
+    // char m_day;
+    // double m_day2;
 };
 struct TEST5
 {
-    char m_day;
-    long double m_day2;
+
+    char q1;
+    // char q11;
+    int q3;
+    short q2;
+    // short q22;
+    int q3;
+    // int q33;
+    long double q4;
+    // char m_day;
+    // long double m_day2;
 };
 void main(void)
 {
